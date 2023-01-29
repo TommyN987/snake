ab59eebd0ecee290c865ef3e8b009af14c638fc1
TommyN987 <tomas.nagy.tn@gmail.com>
2023-01-29 01:16:52 +0100

    add apple and snake length

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from enum import Enum
 import time
+import random
 
 class Direction(Enum):
     UP = 1
@@ -9,41 +10,79 @@ class Direction(Enum):
     LEFT = 3
     RIGHT = 4
 
+SIZE = 40
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load('assets/apple.jpg').convert()
+        self.x = 120
+        self.y = 120
+    
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+    
+    def move(self):
+        self.x = random.randint(1, 25) * SIZE
+        self.y = random.randint(1, 25) * SIZE
+
 class Snake:
-    def __init__(self, surface):
-        self.parent_screen = surface
-        self.block = pygame.image.load('assets/block.jpg').convert()
-        self.x = 100
-        self.y = 100
+    def __init__(self, parent_screen, length):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load('assets/block.jpg').convert()
         self.direction = Direction.DOWN
 
-    def draw(self):
-        self.parent_screen.fill((110, 110, 50))
-
-        self.parent_screen.blit(self.block, (self.x, self.y))
-        pygame.display.flip()
+        self.length = length
+        self.x = [40]*length
+        self.y = [40]*length
 
     def move(self, direction: Direction):
         self.direction = direction
 
     def walk(self, direction: Direction):
+        # update body
+        for i in range(self.length-1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+
+        # update head
         if self.direction == Direction.UP:
-            self.y -= 10
+            self.y[0] -= SIZE
         if self.direction == Direction.DOWN:
-            self.y += 10
+            self.y[0] += SIZE
         if self.direction == Direction.RIGHT:
-            self.x += 10
+            self.x[0] += SIZE
         if self.direction == Direction.LEFT:
-            self.x -= 10
+            self.x[0] -= SIZE
         
         self.draw()
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 50))
+
+        for i in range(self.length):
+            self.parent_screen.blit(self.image, (self.x[i], self.y[i]))
+        
+        pygame.display.flip()
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((500, 500))
-        self.snake = Snake(self.surface)
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.snake = Snake(self.surface, 5)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+    
+    def play(self):
+        self.snake.walk(Direction.DOWN)
+        self.apple.draw()
 
     def run(self):
         running = True
@@ -62,7 +101,7 @@ class Game:
                 elif event.type == QUIT:
                     running = False
             
-            self.snake.walk(self.snake.direction)
+            self.play()
 
             time.sleep(.2)
 
